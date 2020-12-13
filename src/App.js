194eb3815b47a043c96188cab6790e9a08a26dd7
Saros <geosar23@gmail.com>
2020-12-13 17:31:46 +0200

    Added useState to handle Input from the User

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React , {Component} from 'react'
+import React , {useState} from 'react'
 import Navigation from './components/Navigation/navigation.js'
 import Logo from './components/Logo/logo.js'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
@@ -7,13 +7,22 @@ import Particle from './components/Particles/Particle.js'
 import './App.css'
 
 function App() {
+
+  const [inputValue,setInputValue]=useState("");
+
+  const onInputChange = event => {
+    event.preventDefault() //nikos stuff
+    console.log(event.target.value);
+    setInputValue(event.target.value);
+  };
+
   return (
     <div className="App">
         <Particle/>         
         <Navigation/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm/>
+        <ImageLinkForm onChange={onInputChange} value={inputValue}/>
         {/*<FaceRecognition/>*/}
     </div>
   );
