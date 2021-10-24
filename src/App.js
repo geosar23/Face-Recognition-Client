e3b1fe6a256bf5fb3926bd2a3bd5cd1eaee5ee47
George Saramantis <geosar23@gmail.com>
2021-10-24 19:19:03 +0300

    Face Recognition component added , clarifai API installed , on Detect Click added console feedback

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React , {useState} from 'react'
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/navigation.js'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js'
 import Logo from './components/Logo/logo.js'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
 import Rank from './components/Rank/Rank.js'
 import Particle from './components/Particles/Particle.js'
 import './App.css'
+
+const app = new Clarifai.App({
+  apiKey: '9c7d04bafee74c2e852554c07749af15'
+  });
 
 function App() {
 
@@ -19,6 +25,16 @@ function App() {
   const onSubmitButton=(event)=>{
     event.preventDefault()
     console.log('click')
+    app.models
+    .predict("45fb9a671625463fa646c3523a3087d5","https://samples.clarifai.com/face-det.jpg")
+    .then(
+      function(response){
+        console.log(response)
+      },
+      function(err){
+        //there was an error
+      }
+    )
   }
 
 
@@ -32,7 +48,7 @@ function App() {
           onChange={onInputChange} value={inputValue}
           onSubmitButton={onSubmitButton} 
           />
-        {/*<FaceRecognition/>*/}
+        <FaceRecognition/>
     </div>
   );
 }
