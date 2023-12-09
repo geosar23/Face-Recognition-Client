b4211ac0aaf4f3b013392ab1088959897e0bb84f
George <geosar23@gmail.com>
2023-12-09 16:46:31 +0200

    Add reload user data on Admin panel

--- src/Modals/AdminPanelModal/AdminPanelModal.js
+++ src/Modals/AdminPanelModal/AdminPanelModal.js
@@ -19,16 +19,16 @@ function AdminPanelModal({ show, onHide }) {
         const authorizationToken = window.localStorage.getItem('token');
         fetch('/users', {
             headers: {
-            'Content-Type': 'application/json',
-            'Authorization': authorizationToken
+                'Content-Type': 'application/json',
+                'Authorization': authorizationToken
             }
         })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-            setUserData(data)
-        })
-        .catch((error) => console.error('Error fetching user data:', error));
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                setUserData(data)
+            })
+            .catch((error) => console.error('Error fetching user data:', error));
     }
 
 
@@ -38,7 +38,7 @@ function AdminPanelModal({ show, onHide }) {
         setShowDeleteModal(true)
     }
 
-    const closeDeleteModal = () =>  {
+    const closeDeleteModal = () => {
         setShowDeleteModal(false);
     }
 
@@ -47,64 +47,69 @@ function AdminPanelModal({ show, onHide }) {
         getUserData();
     }
 
-  return (
-    <Modal dialogClassName='dark-modal' show={show} size="fullscreen" centered onHide={onHide}>
-        <Modal.Header closeButton>
-            <h3>Admin panel</h3>
-        </Modal.Header>
+    return (
+        <Modal dialogClassName='dark-modal' show={show} size="fullscreen" centered onHide={onHide}>
+            <Modal.Header closeButton>
+                <h3>Admin panel</h3>
+            </Modal.Header>
 
-        <Modal.Body>
-            <DeleteAccountModal show={showDeleteModal} onHide={closeDeleteModal} user={selectedUser} adminAccess={true} onDelete={onDelete}/>
-            <div className="sectionContainer">
-            <h4>Users Management</h4>
-            <table className="table table-striped table-bordered table-dark">
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Password</th>
-                    <th>Entries</th>
-                    <th>Score</th>
-                    <th>Joined</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                {userData.map((user) => (
-                    <tr key={user.id}>
-                    <td>{user.id}</td>
-                    <td>{user.name}</td>
-                    <td>{user.email}</td>
-                    <td>{user.password}</td>
-                    <td>{user.entries}</td>
-                    <td>{user.score}</td>
-                    <td>{moment(user.joined).format()}</td>
-                    <td>
-                        <div>
-                            <button className="m-2 btn btn-sm btn-primary">Edit user</button>
-                            <button className="m-2 btn btn-sm btn-warning">Reset password</button>
-                            {user.name !== 'admin' && (
-                                <button className="m-2 btn btn-sm btn-danger" onClick={() => openConfirmationForDeleteAccountModal(user)}>
-                                    Delete user
-                                </button>
-                            )}
-                        </div>
-                    </td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-            </div>
-        </Modal.Body>
+            <Modal.Body>
+                <DeleteAccountModal show={showDeleteModal} onHide={closeDeleteModal} user={selectedUser} adminAccess={true} onDelete={onDelete} />
+                <div className="sectionContainer">
+                    <div className="flex">
+                        <h4>Users Management</h4>
+                        <span className="pointer" data-bs-toggle="tooltip" data-bs-placement="top" title="Reload users" onClick={() => getUserData()} >
+                            <i className="text-warning fa fa-refresh p-2" aria-hidden="true"></i>
+                        </span>
+                    </div>
+                    <table className="table table-striped table-bordered table-dark">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Password</th>
+                                <th>Entries</th>
+                                <th>Score</th>
+                                <th>Joined</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {userData.map((user) => (
+                                <tr key={user.id}>
+                                    <td>{user.id}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.password}</td>
+                                    <td>{user.entries}</td>
+                                    <td>{user.score}</td>
+                                    <td>{moment(user.joined).format()}</td>
+                                    <td>
+                                        <div>
+                                            <button className="m-2 btn btn-sm btn-primary">Edit user</button>
+                                            <button className="m-2 btn btn-sm btn-warning">Reset password</button>
+                                            {user.name !== 'admin' && (
+                                                <button className="m-2 btn btn-sm btn-danger" onClick={() => openConfirmationForDeleteAccountModal(user)}>
+                                                    Delete user
+                                                </button>
+                                            )}
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </Modal.Body>
 
-        <Modal.Footer>
-            <Button className="btn btn-secondary" onClick={onHide}>
-                Close
-            </Button>
-        </Modal.Footer>
-    </Modal>
-  );
+            <Modal.Footer>
+                <Button className="btn btn-secondary" onClick={onHide}>
+                    Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
 }
 
 export default AdminPanelModal;
