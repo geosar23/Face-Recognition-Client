5cc8490f3ad46fed1240058ffc313d2326069719
George <geosar23@gmail.com>
2023-12-24 15:16:20 +0200

    about page ui improvements

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,9 +15,9 @@ function About({ onRouteChange }) {
                 <div>
                     <h1>Welcome to Face Recognition!</h1>
                     <p>
-                        👋 Welcome to Face Recognition, my exciting project inspired by the renowned online course <a href="https://zerotomastery.io/courses/coding-bootcamp/" target="_blank" rel="noopener noreferrer">"The Complete Web Developer in 2024: Zero to Mastery"</a>.
+                        👋 Welcome to Face Recognition, my exciting project inspired by the renowned online course <a href="https://zerotomastery.io/courses/coding-bootcamp/" target="_blank" rel="noopener noreferrer" title="https://zerotomastery.io/courses/coding-bootcamp/">"The Complete Web Developer in 2024: Zero to Mastery"</a>.
                         I've taken the core concepts and expanded the functionality, creating an interactive space for you to explore and have fun.
-                        In this unique experience, we leverage the power of the <a href="https://www.clarifai.com/" target="_blank" rel="noopener noreferrer">CLARIFAI API</a> for face recognition. To get started, simply register as a user, and then dive into the world of points and face detection!
+                        In this unique experience, we leverage the power of the <a href="https://www.clarifai.com/" target="_blank" rel="noopener noreferrer" title="https://www.clarifai.com/">CLARIFAI API</a> for face recognition. To get started, simply register as a user, and then dive into the world of points and face detection!
                         Earn 1 point for each face detected when you provide a URL to an image. Take it up a notch by uploading your own image file, and score 2 points for every face recognized.
                         The more faces, the more points—challenge yourself and see how high you can go!
                         Ready to embark on this facial recognition journey?
@@ -28,13 +28,20 @@ function About({ onRouteChange }) {
                     </p>
                 </div>
             </div>
-            <div className="row justify-content-center mt-4">
-                <div className="col-sm-5 col-12 shadow p-3 bg-transparent rounded enlarge-on-hover">
-                    <h2> <a href="https://www.linkedin.com/in/georgesaramantis/" target="__blank">
-                        <i className="fa-brands fa-linkedin"></i> LinkedIn</a></h2>
+            <div className="row justify-content-center mt-4 m">
+                <div className="col-sm-5 col-12 m-4 shadow p-3 bg-transparent rounded enlarge-on-hover">
+                    <h2>
+                        <a id="buttonLink" href="https://www.linkedin.com/in/georgesaramantis/" target="__blank">
+                            <i className="fa-brands fa-linkedin"></i> LinkedIn
+                        </a>
+                    </h2>
                 </div>
-                <div className="col-sm-5 col-12 shadow p-3 bg-transparent rounded enlarge-on-hover">
-                    <h2> <a href="https://github.com/geosar23" target="__blank"><i className="fa-brands fa-github"></i> Github</a></h2>
+                <div className="col-sm-5 col-12 m-4 shadow p-3 bg-transparent rounded enlarge-on-hover">
+                    <h2>
+                        <a id="buttonLink" href="https://github.com/geosar23" target="__blank">
+                            <i className="fa-brands fa-github"></i> Github
+                        </a>
+                    </h2>
                 </div>
             </div>
         </div>
